e9148e9e7b3364e537ebee93d8e67179d05b8547
GAMEREWARD <gamereward.io@gmail.com>
2018-06-04 16:10:00 +0700

    no message

---
+++ Assets/GrdSDK/Example/Scripts/PanelDoResetPassword.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelDoResetPassword : MonoBehaviour {
+    public PanelDoResetPassword()
+    {
+        _instance = this;
+    }
+    public InputField tokenField, passwordField, retypePasswordField;
+    public Text messageText;
+    // Use this for initialization
+    static PanelDoResetPassword _instance;
+    public static PanelDoResetPassword Instance { get { return _instance; } }
+    void OnEnable()
+    {
+        messageText.text = "An email was sent to your email. Please check email to get token to change your password!";
+    }
+    public void OnResetClick()
+    {
+        if (tokenField.text.Trim() == "")
+        {
+            messageText.text = "Token can not be empty!. Please check email to get token to change your password!";
+            return;
+        }
+        if (passwordField.text != retypePasswordField.text)
+        {
+            messageText.text = "Retype password is not matched!";
+            return;
+        }
+        Grd.GrdManager.ResetPassword(tokenField.text, passwordField.text,(error,args)=>{
+            if (error == 0)
+            {
+                messageText.text = "YOUR PASSWORD HAD BEEN RESET!";
+            }
+            else
+            {
+                messageText.text = args.ErrorMessage;
+            }
+        });
+    }
+    public void OnMenuClick()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
